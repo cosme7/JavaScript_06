0a8e00240a399b6b5d4ebfdb90c97a18dd418efb
cosme7 <cosmewr@gmail.com>
2022-06-28 03:24:05 -0300

    Correção

--- script.js
+++ script.js
@@ -21,67 +21,55 @@ console.log(dados);
 
 //* Crie um array chamado "cadastro" contendo ao menos 5 objetos. 
 //* Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//* Na propriedade amigos, adicione ao menos 4 itens.
 
 let cadastro = [
     {
         nome: "Cosme",
         idade: "5000",
         telefone: "98623-3888",
-        amigos: ["Felipe Silva", "Izaias"]
+        amigos: ["Felipe Silva", "Izaias","Rafael", "Rian" ]
     },
     {
         nome: "Felipe Silva",
         idade: "6000",
         telefone: "97777-8888",
-        amigos: ["Rafael", "Mariana Melo"]
+        amigos: ["Rafael", "Mariana Melo", "Felipe Correa", "Eliana"]
     },
     {
-        nome: "Rafael",
+        nome: "Eliana",
         idade: "7000",
         telefone: "98888-3333",
-        amigos: ["Felipe Correa", "Luis"]
+        amigos: ["Isabel", "Luis", "Marcus", "Luciana"]
     },
     {
         nome: "Felipe Correa",
         idade: "8000",
         telefone: "93333-2222",
-        amigos: ["Luan", "Marcus"]
+        amigos: ["Gabriel", "Marcus", "Felipe Silva", "Luan", "Lucas Correa"]
     },
     {
         nome: "Mariana Melo",
         idade: "10000",
         telefone: "92222-1111",
-        amigos: ["Eliana", "Mariana Merlin"]
+        amigos: ["Eliana", "Mariana Merlin", "Maria Alessandra", "Mylene"]
     }
 ];
 console.log(cadastro);
 
-//* Na propriedade amigos, adicione ao menos 4 itens.
-
-// console.log(cadastro[1].amigos.unshift("Rian", "Marcus", "Luciana", "Maria Alessandra"));
-console.log(cadastro[0].amigos.splice(1, 0, "Rian", "Marcus", "Lucas Gabriel", "Rafael"));
-// console.log(cadastro[2].amigos.push("Mariana Merlin", "Marcus", "Luciana", "Rafael"));
-cadastro[0].amigos.sort();
-
 //* Mostre no console o nome de um amigo de cada lista.
 
-console.log(`Nome do Amiguinho:`, cadastro[0].amigos[0]);
-console.log(`Nome do Amiguinho:`, cadastro[1].amigos[1]);
-console.log(`Nome do Amiguinho:`, cadastro[2].amigos[0]);
-console.log(`Nome do Amiguinho:`, cadastro[3].amigos[1]);
-console.log(`Nome do Amiguinho:`, cadastro[4].amigos[0]);
+console.log(`Nome do Amiguinho:`, cadastro[0].amigos[3]);
+console.log(`Nome do Amiguinho:`, cadastro[1].amigos[2]);
+console.log(`Nome do Amiguinho:`, cadastro[2].amigos[1]);
+console.log(`Nome do Amiguinho:`, cadastro[3].amigos[0]);
+console.log(`Nome do Amiguinho:`, cadastro[4].amigos[3]);
 
-console.log("===== PERGUNTAR AO PROF =====")
-//* Na propriedade amigos, adicione ao menos 4 itens.
-
-// for (let add = 0; add < cadastro.length; add++){
-//     console.log(cadastro[add].amigos.push("Rian", "Luccas Correa", "Eliana", "Lucas Gabriel"));
-//     cadastro[add].amigos.sort();
-// }
-
+console.log("===== BÔNUS Aula 27/06/2022 =====")
 //* Mostre no console o nome de um amigo de cada lista.
 
-// for(let lista = 0; lista <= 4; lista++){
-//     console.log(cadastro[lista].amigos[lista])
-// }
-console.log("===== PERGUNTAR AO PROF END=====")
+for (let count = 0; count < cadastro.length; count++){
+    console.log("Feito com FOR:", cadastro[count].amigos[0]);
+    cadastro[count].amigos.sort();
+}
+console.log("===== BÔNUS Aula 27/06/2022 =====")
